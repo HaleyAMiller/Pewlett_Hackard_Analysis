b02046b1e461d3cbe8182b3df5e018d1f9e2c658
Haley Miller <haleyxmiller@gmail.com>
2022-03-28 19:49:39 -0400

    updating schema

--- schema.sql
+++ schema.sql
@@ -55,3 +55,17 @@ CREATE TABLE titles (
 );
 
 SELECT * FROM departments;
+
+DROP TABLE dept_emp;
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+SELECT * FROM employees;
